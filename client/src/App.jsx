6c9a9aeae0bdcd568cec0681f0c125e639024c91
Sahil Gurnani <gurnanisahil87@gmail.com>
2024-04-17 14:39:28 +0530

    home pAGE UODATE

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Maps from "./pages/Maps.jsx"
+import Maps from "./pages/Maps.jsx";
 import MyMap from "./pages/MyMap.jsx";
 import Landing from "./pages/Landing.jsx";
 import Login from "./pages/Login.jsx";
@@ -12,17 +12,16 @@ import Home from "./pages/Home.jsx";
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Landing/>}/>
-      <Route path="/maps" element={<Maps />} />
+      <Route path="/" element={<Landing />} />
+      {/* <Route path="/maps" element={<Maps />} /> */}
       <Route path="/signup" element={<Signup />} />
       <Route path="/Login" element={<Login />} />
-      <Route path="/weather" element={<Weather />}/>
+      <Route path="/weather" element={<Weather />} />
       <Route path="/test" element={<Test />} />
-      <Route path="/mapbox" element={<MyMap />}/>
-      <Route path="/Home" element={<Home />}/>
+      <Route path="/mapbox" element={<MyMap />} />
+      <Route path="/Home" element={<Home />} />
     </Routes>
   );
 };
 
 export default App;
- 
